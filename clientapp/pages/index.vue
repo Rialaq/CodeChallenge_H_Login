6b53d03ec00ad010ca25966fc7a1c00f2575bda7
Ricardo J. Almonte <51594905+Rialaq@users.noreply.github.com>
2023-07-08 11:21:50 -0400

    feat: Agregado plantilla ClientApp

---
+++ clientapp/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="content">
+        <div class="loginLeftSide">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            feeds: []
+        }
+    },
+    async mounted() {
+    },
+    methods: {}
+}
+</script>
+
+<style></style>
